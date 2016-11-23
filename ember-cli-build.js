ceb16d07470cd528dfe6843af2099eb589ab03c6
Jonas Knipper <jonas@robocomobo.com>
2016-11-23 18:05:07 +0100

    add moment date package

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,7 @@ module.exports = function(defaults) {
   });
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/moment/min/moment.min.js');
 
 
   return app.toTree();
